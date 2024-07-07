38c7bd4a3e1464bc92e3f4e5414e34260ba8c90b
Hayk <haykooo3@gmail.com>
2024-07-07 18:02:53 +0300

    commit 1.3

--- login.py
+++ login.py
@@ -7,7 +7,7 @@ ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("dark-blue")
 
 app = ctk.CTk()
-app.geometry("350x250+700+350")
+app.geometry("320x250+700+350")
 app.title("Login Page")
 app.resizable(False, False)
 
@@ -81,23 +81,26 @@ label_login = ctk.CTkLabel(frame_login, text="Логин", height=40, font=("Ari
 label_login.grid(row=0, column=0, padx=10, pady=20)
 
 entry_login = ctk.CTkEntry(frame_login, placeholder_text="Логин", height=40, font=("Arial", 16))
-entry_login.grid(row=0, column=1, pady=20)
+entry_login.grid(row=0, column=1, pady=20, sticky="we")
 
 label_password = ctk.CTkLabel(frame_login, text="Пароль", height=40, font=("Arial", 16))
 label_password.grid(row=1, column=0, padx=10)
 
 entry_password = ctk.CTkEntry(frame_login, show="*", placeholder_text="Пароль", height=40, font=("Arial", 16))
-entry_password.grid(row=1, column=1)
+entry_password.grid(row=1, column=1, sticky="we")
 
 show_image = ctk.CTkImage(Image.open("img/show.png"), size=(30, 30))
 hide_image = ctk.CTkImage(Image.open("img/hide.png"), size=(30, 30))
+log_image = ctk.CTkImage(Image.open("img/log.png"), size=(25, 25))
+reg_image = ctk.CTkImage(Image.open("img/reg.png"), size=(25, 25))
 
 button_show_password = ctk.CTkButton(frame_login, text="", height=30, width=30, image=show_image,
                                      command=lambda: show_password(entry_password, button_show_password))
 button_show_password.grid(row=1, column=2, padx=10)
 
-button_login = ctk.CTkButton(frame_login, text="Войти", height=40, font=("Arial", 16), command=login)
-button_login.grid(row=2, column=1, padx=20, pady=20)
+button_login = ctk.CTkButton(frame_login, text="     Войти", height=40, font=("Arial", 16), command=login,
+                             image=log_image, anchor="w", width=160)
+button_login.grid(row=2, column=1, pady=20, sticky="ew")
 
 frame_register = ctk.CTkFrame(app)
 frame_register.pack(padx=10, pady=10, fill="both", expand=True)
@@ -105,24 +108,24 @@ frame_register.pack(padx=10, pady=10, fill="both", expand=True)
 label_login_register = ctk.CTkLabel(frame_register, text="Логин", height=40, font=("Arial", 16))
 label_login_register.grid(row=0, column=0, padx=10, pady=20)
 
-entry_login_register = ctk.CTkEntry(frame_register, placeholder_text="Логин", height=40, font=("Arial", 16))
-entry_login_register.grid(row=0, column=1, pady=20)
+entry_login_register = ctk.CTkEntry(frame_register, placeholder_text="Логин", height=40, font=("Arial", 16), )
+entry_login_register.grid(row=0, column=1, pady=20, sticky="we")
 
 label_password_register = ctk.CTkLabel(frame_register, text="Пароль", height=40, font=("Arial", 16))
 label_password_register.grid(row=1, column=0, padx=10)
 
 entry_password_register = ctk.CTkEntry(frame_register, show="*", placeholder_text="Пароль", height=40,
                                        font=("Arial", 16))
-entry_password_register.grid(row=1, column=1)
+entry_password_register.grid(row=1, column=1, sticky="we")
 
 button_show_password_register = ctk.CTkButton(frame_register, text="", height=30, width=30, image=show_image,
                                               command=lambda: show_password(entry_password_register,
                                                                             button_show_password_register))
 button_show_password_register.grid(row=1, column=2, padx=10)
 
-button_register = ctk.CTkButton(frame_register, text="Регистрация", height=40, font=("Arial", 16),
-                                command=register)
-button_register.grid(row=2, column=1, padx=20, pady=20)
+button_register = ctk.CTkButton(frame_register, text="Регистрация", height=40, font=("Arial", 16), image=reg_image,
+                                command=register, anchor="w", width=160)
+button_register.grid(row=2, column=1, pady=20, sticky="we")
 
 if __name__ == '__main__':
     segmented_button_callback("Авторизация")
